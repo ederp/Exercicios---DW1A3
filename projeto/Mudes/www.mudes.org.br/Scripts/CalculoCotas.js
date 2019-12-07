2fbb620970eb571d3aed39f08d8c4d5d18028cd6
Eder Pereira <epmutley@gmail.com>
2019-12-06 23:48:20 -0300

    Add files via upload

---
+++ projeto/Mudes/www.mudes.org.br/Scripts/CalculoCotas.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    $('input').change(function () {
+        if ($(this).val() == "") {
+            $(this).val(0);
+        }
+
+        var totalExclusao = parseInt($('#exclusaoFormacao').val()) + parseInt($('#exclusaoDirecao').val()) + parseInt($('#exclusaoTemporario').val()) + parseInt($('#exclusaoAprendizes').val());
+        var saldo = parseInt($('#qtdEmpregados').val()) - totalExclusao;
+        if (saldo < 0) {
+            saldo = 0;
+        }
+        var cotaMinima = saldo * 0.05;
+        var cotaMaxima = saldo * 0.15;
+
+        $("#TotalExclusao").text(totalExclusao);
+        $("#Saldo").text(saldo);
+        $("#CotaMinima").text(cotaMinima.toFixed(1));
+        $("#CotaMaxima").text(cotaMaxima.toFixed(1));
+    });
+});
